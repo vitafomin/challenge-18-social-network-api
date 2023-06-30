8bfde030d2555a5e43ac453d1c5de5cde237fe73
Vita Fomin <vita.bukhalo@gmail.com>
2023-06-30 13:01:35 -0400

    routes folder and files set up and complete

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,18 @@
+const router = require("express").Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction,
+} = require("../../controllers/thoughtController.js");
+
+router.route("/").get(getThoughts).post(createThought);
+
+router.route("/:thoughtId").get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(createReaction);
+
+router.route("/:thoughtId/reactions/:assignmentsId").delete(deleteReaction);
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+} = require("../../controllers/userController.js");
+
+router.route("/").get(getUsers).post(createUser);
+
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route("/:userId/friends").post(addFriend);
+
+router.route("/:userId/friends/:friendsId").delete(deleteFriend);
+
+module.exports = router;
