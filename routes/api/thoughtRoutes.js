12d93970416836c2b732a36dbb26d6f866bcb3b0
Vita Fomin <vita.bukhalo@gmail.com>
2023-07-03 23:18:10 -0400

    cleaned up code in routes folder

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,17 +1,21 @@
 const router = require("express").Router();
 const {
-    getThoughts,
-    getSingleThought,
-    createThought,
-    updateThought,
-    deleteThought,
-    createReaction,
-    deleteReaction,
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
 } = require("../../controllers/thoughtController.js");
 
 router.route("/").get(getThoughts).post(createThought);
 
-router.route("/:thoughtId").get(getSingleThought).put(updateThought).delete(deleteThought);
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
 
 router.route("/:thoughtId/reactions").post(createReaction);
 
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
 const {
-    getUsers,
-    getSingleUser,
-    createUser,
-    updateUser,
-    deleteUser,
-    addFriend,
-    deleteFriend,
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
 } = require("../../controllers/userController.js");
 
 router.route("/").get(getUsers).post(createUser);
