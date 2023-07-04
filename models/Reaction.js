fc9d8ec539f444fe2c3f401a33693a22d7063b55
Vita Fomin <vita.bukhalo@gmail.com>
2023-07-03 23:17:11 -0400

    cleaned up code in models folder

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,32 +1,31 @@
 const { Schema, Types } = require("mongoose");
 
 const reactionSchema = new Schema(
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId(),
-        },
-        reactionBody: {
-            type: String,
-            required: true,
-            maxlength: 280,
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            // get: (date) => {timesince(date)}
-        },
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
     },
-    {
-        toJSON: {
-            getters: true,
-        },
-        timestamps: true,
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
     },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    timestamps: true,
+  }
 );
 
 module.exports = reactionSchema;
--- models/User.js
+++ models/User.js
@@ -1,42 +1,42 @@
 const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema(
-    {
-       username: {
-        type: String,
-        unique: true,
-        required: true,
-        trimmed: true
-       } ,
-       email: {
-        type: String,
-        required: true,
-        unique: true,
-       // match: [/"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}i"/],
-       },
-       thoughts: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "Thought"
-        },
-       ],
-       friends: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: "User"
-        },
-       ],
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
     },
-    {
-        toJSON: {
-            virtuals: true,
-        },
-        id: false
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      // match: [/"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}i"/],
     },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
 );
 
 userSchema.virtual("friendCount").get(function () {
-    return this.friends.length;
+  return this.friends.length;
 });
 
 const User = model("user", userSchema);
